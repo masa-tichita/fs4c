0bf9d154acfc7845ea2fc3ddd074f4de27541a9b
masa-tichita <bunmamasa@gmail.com>
2025-09-30 18:22:22 +0900

    Reorder import statements in main.py for consistency

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
-from utils.logging import setup_logger, log_fn
+
+from utils.logging import log_fn, setup_logger
 from utils.system import project_root
 
 setup_logger()
+
 
 # TODO: 挙動の確認のためのスクリプトあとで消す
 @log_fn
@@ -10,6 +12,7 @@ class User(BaseModel):
     id: int
     name: str
     is_active: bool
+
 
 user = User(id=1, name="John", is_active=True)
 print(user.model_dump())
